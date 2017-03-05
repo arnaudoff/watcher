f173df9561a046b15f2f13a1ff3ea6d8603566b2
Ivaylo Arnaudov <ivaylo.arnaudov12@gmail.com>
2017-03-05 09:19:24 +0200

    Implement create and update for sensors views

--- sensors/views.py
+++ sensors/views.py
@@ -1,10 +1,21 @@
 from django.shortcuts import render, get_object_or_404
-from django.http import HttpResponse
+from django.http import HttpResponse, HttpResponseRedirect
 
+from .forms import SensorForm
 from .models import Sensor
 
 def sensor_create(request):
-    return HttpResponse("<h1>Sensors home</h1>")
+    form = SensorForm(request.POST or None)
+    if form.is_valid():
+        sensor = form.save(commit=False)
+        sensor.save()
+        return HttpResponseRedirect(sensor.get_absolute_url())
+
+    context = {
+        "form": form
+    }
+
+    return render(request, "sensors/create.html", context)
 
 def sensor_detail(request, id=None):
     sensor = get_object_or_404(Sensor, id=id)
@@ -13,7 +24,7 @@ def sensor_detail(request, id=None):
         "sensor": sensor
     }
 
-    return render(request, "detail.html", context)
+    return render(request, "sensors/detail.html", context)
 
 def sensor_list(request):
     sensors = Sensor.objects.all()
@@ -22,10 +33,23 @@ def sensor_list(request):
         "sensors": sensors
     }
 
-    return render(request, "index.html", context)
+    return render(request, "sensors/index.html", context)
 
-def sensor_update(request):
-    return HttpResponse("<h1>Sensors home</h1>")
+def sensor_update(request, id=None):
+    sensor = get_object_or_404(Post, id=id)
+
+    form = SensorForm(request.POST or None, instance=sensor)
+    if form.is_valid():
+        sensor = form.save(commit=False)
+        sensor.save()
+        return HttpResponseRedirect(sensor.get_absolute_url())
+
+    context = {
+        "sensor": sensor
+        "form": form
+    }
+
+    return render(request, "sensors/update.html", context)
 
 def sensor_delete(request):
     return HttpResponse("<h1>Sensors home</h1>")
