7a2588226e0148d4fd4749db938e171b6b2f8bd7
DieEin <DieEin>
2017-04-01 23:17:52 +0300

    input password twice in registration

--- users/forms.py
+++ users/forms.py
@@ -29,6 +29,7 @@ class UserRegisterForm(forms.ModelForm):
 	email = forms.EmailField(label='Email address')
 	email_confirm = forms.EmailField(label='Confirm Email')
 	password = forms.CharField(widget=forms.PasswordInput)
+	password_confirm = forms.CharField(widget=forms.PasswordInput, label='Confirm password')
 
 	class Meta:
 		model = User
@@ -36,14 +37,13 @@ class UserRegisterForm(forms.ModelForm):
 			'username',
 			'email',
 			'email_confirm',
-			'password'
+			'password',
+			'password_confirm'
 		]
 
 	def clean_email_confirm(self):
-		print(self.cleaned_data)
 		email = self.cleaned_data.get('email')
 		email_confirm = self.cleaned_data.get('email_confirm')
-		print(email, email_confirm)
 
 		if email != email_confirm:
 			raise forms.ValidationError("Emails must match!")
@@ -53,3 +53,12 @@ class UserRegisterForm(forms.ModelForm):
 			raise forms.ValidationError("This email has already been registered!")
 
 		return email
+
+	def clean_password_confirm(self):
+		password = self.cleaned_data.get('password')
+		password_confirm = self.cleaned_data.get('password_confirm')
+
+		if password != password_confirm:
+			raise forms.ValidationError("Passwords must match!")
+
+		return password
