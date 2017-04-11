ef0c742613d49e2f09f85fbf95a39b8c14af22d1
DieEin <DieEin>
2017-04-11 08:31:45 +0300

    welcome [username] text on dashboard

--- dashboard/views.py
+++ dashboard/views.py
@@ -1,7 +1,15 @@
 from django.shortcuts import render
+from sensors.models import Sensor
 
 def dashboard_view(request):
+	print(request.user.is_authenticated())
+	print(Sensor.objects.all())
 
-	context = {}
+	username = None
+	if request.user.is_authenticated():	
+		username = request.user.username
+	context = {
+		"username": username
+	}
 
 	return render(request, "dashboard.html", context)
