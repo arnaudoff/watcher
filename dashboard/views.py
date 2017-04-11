8cb74037cf510e54437b12f55ac4dbb17ed442fa
DieEin <DieEin>
2017-04-11 11:57:04 +0300

    dashboard shows all sensors added by the logged in user

--- dashboard/views.py
+++ dashboard/views.py
@@ -1,15 +1,20 @@
 from django.shortcuts import render
 from sensors.models import Sensor
+from django.contrib.auth.models import User
 
 def dashboard_view(request):
 	print(request.user.is_authenticated())
 	print(Sensor.objects.all())
 
 	username = None
+	test = None
 	if request.user.is_authenticated():	
 		username = request.user.username
+		test = Sensor.objects.filter(user=request.user)
+
 	context = {
-		"username": username
+		'username': username,
+		'sensor_results': test
 	}
 
 	return render(request, "dashboard.html", context)
