e7cdbf0c5a01aef4c2a58398a6d3441bbf55714e
DieEin <DieEin>
2017-04-25 09:06:24 +0300

    implemented unique sensor field validation

--- dashboard/sensors/models.py
+++ dashboard/sensors/models.py
@@ -3,7 +3,7 @@ from django.core.urlresolvers import reverse
 from django.contrib.auth.models import User
 
 class Sensor(models.Model):
-    name = models.CharField(max_length=20)
+    name = models.CharField(max_length=20, unique=True)
     timestamp = models.DateTimeField(auto_now=False, auto_now_add=True)
     user = models.ForeignKey(User, default='')
     active = models.BooleanField(default=True)
